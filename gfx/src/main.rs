f29abdf2431660dc433732a7b572f65370ed5537
2LeoCode <leo.13suardi@outlook.com>
2026-01-29 23:45:30 +0100

    advanced use of graphical features

--- gfx/src/main.rs
+++ gfx/src/main.rs
@@ -1,13 +1,25 @@
 mod raylib;
-use raylib::wrapper::{RaylibColor, init_window};
+
+use raylib::wrapper::{Color, init_window};
 
 fn main() {
-  let window = init_window(800, 600, "urmom").expect("Failed to create window");
+  const WIDTH: i32 = 800;
+  const HEIGHT: i32 = 600;
+  let window = init_window(WIDTH, HEIGHT, "zappy").expect("Invalid arguments to init_window");
+  let current_monitor = window.get_current_monitor();
 
-  window.set_target_fps(60);
-
+  window.set_target_fps(current_monitor.get_refresh_rate());
   while !window.should_close() {
-    let drawer = window.begin_drawing();
-    drawer.clear_background(RaylibColor::Red);
+    let pen = window.begin_drawing();
+    pen.clear_background(Color::Red);
+    pen
+      .draw_text(
+        "urmom",
+        WIDTH / 2 - (2.5 * HEIGHT as f32 / 16.) as i32,
+        HEIGHT / 2 - HEIGHT / 16,
+        HEIGHT / 8,
+        Color::Blue,
+      )
+      .expect("Invalid arguments to draw_text");
   }
 }
--- gfx/src/raylib/wrapper.rs
+++ gfx/src/raylib/wrapper.rs
@@ -1,7 +1,8 @@
 use {
   super::bindings::InitWindow,
   crate::raylib::bindings::{
-    BeginDrawing, ClearBackground, CloseWindow, Color, EndDrawing, SetTargetFPS, WindowShouldClose,
+    self, BeginDrawing, ClearBackground, CloseWindow, DisableCursor, DrawText, EnableCursor,
+    EndDrawing, GetCurrentMonitor, GetMonitorRefreshRate, SetTargetFPS, Vector3, WindowShouldClose,
   },
   std::ffi::{CString, NulError, c_int},
   thiserror::Error,
@@ -13,7 +14,13 @@ pub enum InitWindowError {
   TitleContainsNul(#[from] NulError),
 }
 
-pub enum RaylibColor {
+#[derive(Error, Debug)]
+pub enum DrawTextError {
+  #[error("nul character in text")]
+  TextContainsNul(#[from] NulError),
+}
+
+pub enum Color {
   LightGray,
   Gray,
   DarkGray,
@@ -40,174 +47,186 @@ pub enum RaylibColor {
   Blank,
   Magenta,
   RayWhite,
+  Custom(u8, u8, u8, u8),
 }
 
-impl From<RaylibColor> for Color {
-  fn from(value: RaylibColor) -> Color {
-    use RaylibColor::*;
+impl From<Color> for bindings::Color {
+  fn from(value: Color) -> bindings::Color {
+    use Color::*;
     match value {
-      LightGray => Color {
+      LightGray => bindings::Color {
         r: 200,
         g: 200,
         b: 200,
         a: 255,
       },
-      Gray => Color {
+      Gray => bindings::Color {
         r: 130,
         g: 130,
         b: 130,
         a: 255,
       },
-      DarkGray => Color {
+      DarkGray => bindings::Color {
         r: 80,
         g: 80,
         b: 80,
         a: 255,
       },
-      Yellow => Color {
+      Yellow => bindings::Color {
         r: 253,
         g: 249,
         b: 0,
         a: 255,
       },
-      Gold => Color {
+      Gold => bindings::Color {
         r: 255,
         g: 203,
         b: 0,
         a: 255,
       },
-      Orange => Color {
+      Orange => bindings::Color {
         r: 255,
         g: 161,
         b: 0,
         a: 255,
       },
-      Pink => Color {
+      Pink => bindings::Color {
         r: 255,
         g: 109,
         b: 194,
         a: 255,
       },
-      Red => Color {
+      Red => bindings::Color {
         r: 230,
         g: 41,
         b: 55,
         a: 255,
       },
-      Maroon => Color {
+      Maroon => bindings::Color {
         r: 190,
         g: 33,
         b: 55,
         a: 255,
       },
-      Green => Color {
+      Green => bindings::Color {
         r: 0,
         g: 228,
         b: 48,
         a: 255,
       },
-      Lime => Color {
+      Lime => bindings::Color {
         r: 0,
         g: 158,
         b: 47,
         a: 255,
       },
-      DarkGreen => Color {
+      DarkGreen => bindings::Color {
         r: 0,
         g: 117,
         b: 44,
         a: 255,
       },
-      SkyBlue => Color {
+      SkyBlue => bindings::Color {
         r: 102,
         g: 191,
         b: 255,
         a: 255,
       },
-      Blue => Color {
+      Blue => bindings::Color {
         r: 0,
         g: 121,
         b: 241,
         a: 255,
       },
-      DarkBlue => Color {
+      DarkBlue => bindings::Color {
         r: 0,
         g: 82,
         b: 172,
         a: 255,
       },
-      Purple => Color {
+      Purple => bindings::Color {
         r: 200,
         g: 122,
         b: 255,
         a: 255,
       },
-      Violet => Color {
+      Violet => bindings::Color {
         r: 135,
         g: 60,
         b: 190,
         a: 255,
       },
-      DarkPurple => Color {
+      DarkPurple => bindings::Color {
         r: 112,
         g: 31,
         b: 126,
         a: 255,
       },
-      Beige => Color {
+      Beige => bindings::Color {
         r: 211,
         g: 176,
         b: 131,
         a: 255,
       },
-      Brown => Color {
+      Brown => bindings::Color {
         r: 127,
         g: 106,
         b: 79,
         a: 255,
       },
-      DarkBrown => Color {
+      DarkBrown => bindings::Color {
         r: 76,
         g: 63,
         b: 47,
         a: 255,
       },
-      White => Color {
+      White => bindings::Color {
         r: 255,
         g: 255,
         b: 255,
         a: 255,
       },
-      Black => Color {
+      Black => bindings::Color {
         r: 0,
         g: 0,
         b: 0,
         a: 255,
       },
-      Blank => Color {
+      Blank => bindings::Color {
         r: 0,
         g: 0,
         b: 0,
         a: 0,
       },
-      Magenta => Color {
+      Magenta => bindings::Color {
         r: 255,
         g: 0,
         b: 255,
         a: 255,
       },
-      RayWhite => Color {
+      RayWhite => bindings::Color {
         r: 245,
         g: 245,
         b: 245,
         a: 255,
       },
+      Custom(r, g, b, a) => bindings::Color { r, g, b, a },
     }
   }
 }
 
-pub struct Window;
-pub struct Drawer;
+pub struct Window {
+  title: CString,
+}
+pub struct Pen;
+pub struct Monitor(i32);
+pub struct Camera3D {
+  pub position: Vector3,
+  pub target: Vector3,
+  pub up: Vector3,
+  pub fovy: f32,
+  pub projection: i32,
+}
 
 impl Drop for Window {
   fn drop(&mut self) {
@@ -217,10 +236,34 @@ impl Drop for Window {
   }
 }
 
-impl Drop for Drawer {
+impl Drop for Pen {
   fn drop(&mut self) {
     unsafe {
       EndDrawing();
+    }
+  }
+}
+
+impl From<bindings::Camera3D> for Camera3D {
+  fn from(value: bindings::Camera3D) -> Self {
+    Camera3D {
+      position: value.position,
+      target: value.target,
+      up: value.up,
+      fovy: value.fovy,
+      projection: value.projection as i32,
+    }
+  }
+}
+
+impl From<Camera3D> for bindings::Camera3D {
+  fn from(value: Camera3D) -> Self {
+    bindings::Camera3D {
+      position: value.position,
+      target: value.target,
+      up: value.up,
+      fovy: value.fovy,
+      projection: value.projection as c_int,
     }
   }
 }
@@ -230,11 +273,23 @@ impl Window {
     unsafe { WindowShouldClose() }
   }
 
-  pub fn begin_drawing(&self) -> Drawer {
+  pub fn begin_drawing(&self) -> Pen {
     unsafe {
       BeginDrawing();
     }
-    Drawer {}
+    Pen {}
+  }
+
+  pub fn enable_cursor(&self) {
+    unsafe {
+      EnableCursor();
+    }
+  }
+
+  pub fn disable_cursor(&self) {
+    unsafe {
+      DisableCursor();
+    }
   }
 
   pub fn set_target_fps(&self, fps: i32) {
@@ -242,19 +297,47 @@ impl Window {
       SetTargetFPS(fps as c_int);
     }
   }
+
+  pub fn get_current_monitor(&self) -> Monitor {
+    unsafe { Monitor(GetCurrentMonitor() as i32) }
+  }
 }
 
-impl Drawer {
-  pub fn clear_background(&self, color: RaylibColor) {
+impl Pen {
+  pub fn clear_background(&self, color: Color) {
     unsafe {
       ClearBackground(color.into());
     }
   }
+
+  pub fn draw_text(
+    &self,
+    text: &str,
+    pos_x: i32,
+    pos_y: i32,
+    font_size: i32,
+    color: Color,
+  ) -> Result<(), DrawTextError> {
+    let text = CString::new(text)?;
+
+    unsafe {
+      DrawText(text.as_ptr(), pos_x, pos_y, font_size, color.into());
+    }
+    Ok(())
+  }
+}
+
+impl Monitor {
+  pub fn get_refresh_rate(&self) -> i32 {
+    unsafe { GetMonitorRefreshRate(self.0 as c_int) }
+  }
 }
 
 pub fn init_window(width: i32, height: i32, title: &str) -> Result<Window, InitWindowError> {
+  let title = CString::new(title)?;
+
   unsafe {
-    InitWindow(width, height, CString::new(title)?.as_ptr());
+    InitWindow(width as c_int, height as c_int, title.as_ptr());
   }
-  Ok(Window {})
+  Ok(Window { title })
 }
